23d975c2efc0d51f8a17f400b44d420dbda9aded
Sergeevich <mrssn84@mail.ru>
2022-09-14 20:38:22 +0300

    HomeWork -1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="display">
-		<input v-model="operand1"/>
-		{{ operator }}
-		<input v-model="operand2"/>
-		= {{ result }} <br>
-		<button @click="operator = '+'">+</button>
-		<button @click="operator = '-'">-</button>
-		<button @click="operator = '*'">*</button>
-		<button @click="operator = '/'">/</button>
-		<button @click="operator = this.result">=</button>
+		<div class="screen">
+			<input @keyup.enter="calc()" type="text" v-model="result" placeholder="0">
+		</div>
+		<div class="keyboard">
+			
+      <div class="calc-num">
+				<button @click="input(num)" class="cell num" v-for="num in numbers">{{num}}</button>
+			</div>
+      <button @click="input(op)" class="cell op" v-for="op in operators">{{op}}</button>
+      <button @click="reset()" class="cell op">R</button>
+      <button @click="calc()" class="cell op">=</button>
+		</div>
 
 	</div>
 		
@@ -20,27 +23,68 @@ export default {
   name: 'App',
   	data() {
 			return {
-			operand1: 0,
-			operand2: 0,
-			operator: '+',
-			result: 0
+			numbers: [1,2,3,4,5,6,7,8,9,0],
+			operators: ['+','-','*','/','**'],
+			result: ''
 		}
 	},
 	methods: {
-		getResult() {
-			switch (operator) {
-				case this.operator == '+':
-					this.result = Number(this.operand1) + Number(this.operand2)
-					break;
-			
-				default: result = "Error"
-					break;
-			}
-		}
+		input: function(char) {
+      this.result = this.result.toString();
+      this.result+=char;
+    },
+		reset: function() {
+      this.result = '';
+    },
+    calc: function() {
+      this.result = eval(this.result);
+    }	
+		
 	}
 }
 </script>
 
 <style>
-
+	* {
+		font-family: 'Gilroy',sans-serif;
+		transition: 0.3s all;
+			font-weight: 600;
+	}
+	.display {
+		width: 200px;
+		box-shadow: 0 3px 15px grey;
+	}
+	input[type="text"] {
+		width: 190px;
+		border: 0;
+		color: #243849;
+		font-size: 18px;
+		padding: 5px 5px;
+		text-align: right;
+	}
+	.keyboard, .calc-num {
+		display: flex;                       
+		flex-wrap: wrap;  
+		justify-content: space-around;
+		width: 100%;
+		
+	}
+	.cell {
+		flex: 33%;                       
+		height: 50px;
+		font-size: 18px;
+		border: none;
+	}
+	.num {
+		background-color: #243849;
+		color: #fff;
+		font-weight: 400;
+	}
+	.cell:hover{
+		background-color: #182530;
+	}
+	.op {
+		background-color: #3cff00;
+		color: #243849;
+	}
 </style>
